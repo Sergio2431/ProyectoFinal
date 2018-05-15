e1f5e9eb7a09db5ebdef1ae1147647c03a32973b
regi <reginoboschlozano@gmail.com>
2018-05-15 10:10:06 +0200

    Projecto Actualizado 15/05

---
+++ Proyecto/ProjectoIntegrado/public/js/listadoUsuarios.js
@@ -0,0 +1,54 @@
+var type = "";
+
+var busqueda="";
+
+
+
+$(document).ready(function()
+{
+  //para cojer el valor inicial para el Update
+  $( 'input').focusin(function()
+  { //valor inical del input
+    busqueda=$(this).attr('value')
+
+    $('input').focusout(function()
+    {
+
+       type = $(this).attr('name');
+
+      document.getElementById(type).style.borderColor = "#E0D68F";
+      document.getElementById("registro").disabled = false;
+
+              var envio = $(this).val();
+              var dataString = 'busqueda='+type+"-"+envio;
+
+
+              console.log(busqueda+"-----"+envio);
+              //comprobamos el nombre Inicial y el actual antes de comprobarDatos
+              //si es el mismo no hacemos nodoImagenEditar
+              if(busqueda != envio)
+              if(type != "curso" && type != "edad" &type != "apellidos")
+              {
+                $.ajax({
+                    type: "POST",
+                    url: "../../src/seguridad/PHP/comprobarDatos.php",
+                    data: dataString,
+                    success: function(data)
+                    {
+                        if(data == 0 && $("#"+type).val().length > 0)
+                        {
+                          document.getElementById(type).style.borderColor = "green";
+                          document.getElementById("registro").disabled = false;
+                        }
+                          else if(data == 1 && $("#"+type).val().length > 0)
+                          {
+                            document.getElementById(type).style.borderColor = "red";
+                            document.getElementById("registro").disabled = true;
+
+                          }
+                    }
+                });
+            }
+      });
+    });
+  });
---
+++ Proyecto/ProjectoIntegrado/src/seguridad/comprobarDatos.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+      function ComprobarConexion()
+      {
+        $this->conector = new mysqli("localhost","root","","juegos");
+        if($this->conector ->connect_errno)
+        {
+          echo "Fallo al conectar a MySQL: ".$this->conector->connect_error;
+        }
+        else
+        {
+          $this->ComprobarDatos();
+        }
+      }
+      function ComprobarDatos()
+      {
+        $recibido =$_POST["busqueda"];
+        $busqueda = explode("-", $recibido);
+        $resultado = $this->conector->query("Select * from Usuarios where $busqueda[0] = '$busqueda[1]'");
+        $contar = mysqli_num_rows($resultado);
+        $json_string = json_encode($contar);
+        echo $json_string;
+      }
+
+      */
+
+
+?>
