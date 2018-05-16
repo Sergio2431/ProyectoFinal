6050fe58f4ef4f1c41cbb73bbd8576effd2e3227
ALUMNADO\jolapu <joseplajara7@gmail.com>
2018-05-16 11:44:00 +0200

    subir archivos para crear ofertas hasta post

---
+++ Proyecto/ProyectoIntegrado/src/models/recibir/crear_oferta.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombre=$_POST['Nombre'];
+$horas=$_POST['Horas'];
+$precio=$_POST['Precio'];
+$descripcion=$_POST['Descripcion'];
+
+
+echo("NOMBRE: ".$nombre."HORAS: ".$horas."PRECIO: ".$precio."DESCRIPCION: ".$descripcion);
+?>
