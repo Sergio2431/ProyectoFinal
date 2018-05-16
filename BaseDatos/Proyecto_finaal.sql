95a01fb45442ca11153c892d7647f094d85621bc
regi <reginoboschlozano@gmail.com>
2018-05-16 10:41:26 +0200

    a

--- BaseDatos/Proyecto_finaal.sql
+++ BaseDatos/Proyecto_finaal.sql
@@ -1,18 +1,4 @@
-﻿-- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.1.21-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.4.0.5125
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- Volcando estructura de base de datos para proyecto_integrado(freelancers)
+﻿
 CREATE DATABASE IF NOT EXISTS `proyecto_integrado(freelancers)` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `proyecto_integrado(freelancers)`;
 
