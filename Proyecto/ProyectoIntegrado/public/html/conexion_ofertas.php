ba832db6307301b4fc0818344a33ce4b50ee09af
ALUMNADO\seruba <sergio_311096@hotmail.es>
2018-05-21 12:31:30 +0200

    subir cambios

--- Proyecto/ProyectoIntegrado/public/html/conexion_ofertas.php
+++ Proyecto/ProyectoIntegrado/public/html/conexion_ofertas.php
@@ -4,13 +4,13 @@ $conector = new mysqli("localhost", "root", "", "proyecto_integrado(freelancer)"
 if ($conector->connect_errno) {
   echo "Fallo al conectar a MySQL: " . $conector->connect_error;
 }else{
-  $oferta_update=$_POST['seleccionar_oferta'];
+  $oferta_select=$_POST['seleccionar_oferta'];
   $oferta=$_POST['Oferta'];
 
   //buscar las ofertas del tipo seleccionado
-  echo "oferta_search ".$oferta_update."Oferta ".$oferta;
+  echo "oferta_search ".$oferta_select."Oferta ".$oferta;
 
-  $conector->query("UPDATE usuario SET oferta='$oferta' WHERE oferta='$oferta_update'");
+  $conector->query("UPDATE tipo_oferta SET oferta='$oferta' WHERE oferta='$oferta_select'");
 
   }
   header('Location: listado_ofertas.php');
