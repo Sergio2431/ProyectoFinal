bf0b46b808e38561456b336d896d40c5577cbb73
andreumas <andreumasandres1995@gmail.com>
2018-05-21 12:32:53 +0200

    integrar Active directory

---
+++ Proyecto/ProyectoIntegrado/public/html/conexion.php
@@ -0,0 +1,21 @@
+<?php
+    $adServer = "ldap://10.2.72.21";
+
+    $ldap = ldap_connect($adServer);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $ldaprdn = 'SERGIO' . "\\" . $username;
+    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+    $bind = @ldap_bind($ldap, $ldaprdn, $password);
+    if ($bind) {
+    	$msg = "Estás logueado como correctamente como $username";
+
+      header('Location: borrarUsuario.html');
+
+    } else {
+        $msg = "Usuario o contraseña incorrectos";
+    }
+    echo $msg;
+ ?>
