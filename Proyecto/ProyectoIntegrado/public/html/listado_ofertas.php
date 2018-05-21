371ce39511c252ece03342f932f1eae12a944cd8
ALUMNADO\seruba <sergio_311096@hotmail.es>
2018-05-21 12:57:13 +0200

    subir cambios

--- Proyecto/ProyectoIntegrado/public/html/listado_ofertas.php
+++ Proyecto/ProyectoIntegrado/public/html/listado_ofertas.php
@@ -41,7 +41,7 @@
 
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
-            <a href="../index.html">Home</a>
+            <a href="../index.html">Inicio</a>
           </li>
           <li class="breadcrumb-item active">Ofertas</li>
         </ol>
